1082f760d5eab36b8cf4362826feab4281675137
akrauss419 <akrauss419@gmail.com>
2022-11-21 15:08:40 -0800

    authentication & dynamic nav bar

--- main_app/models.py
+++ main_app/models.py
@@ -28,7 +28,7 @@ class User(models.Model):
 
 class Contractor(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.IntegerField(max_length=10)
+    phone = models.IntegerField()
     email = models.EmailField(max_length=150)
     location = models.CharField(max_length=150)
     rating = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import Job, User, Review, Contractor, JobPhoto, UserPhoto, ContractorPhoto
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+@login_required
+def jobs_index(request):
+  jobs = Job.objects.filter(posters=request.user)
+  return render(request, 'jobs/index.html', {
+    'jobs': jobs
+  })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
