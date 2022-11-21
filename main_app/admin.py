a5191790c647781d797746f06e9ffb0f90521513
RuslanDidun <Ndivision5@gmail.com>
2022-11-21 15:10:26 -0700

    models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Job, User, Review, Contractor, JobPhoto, UserPhoto, ContractorPhoto
 
 # Register your models here.
+admin.site.register(Job)
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Contractor)
+admin.site.register(JobPhoto)
+admin.site.register(UserPhoto)
+admin.site.register(ContractorPhoto)
