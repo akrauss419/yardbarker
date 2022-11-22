52924797876ea90b8d20264b8e484c3283f0b043
akrauss419 <akrauss419@gmail.com>
2022-11-21 16:57:13 -0800

    jobs detail & create job form

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('jobs/', views.jobs_index, name='index'),
+  path('jobs/<int:job_id>/', views.jobs_detail, name='detail'),
+  path('jobs/create/', views.JobCreate.as_view(), name='jobs_create'),
   path('accounts/signup/', views.signup, name='signup'),
 ]
