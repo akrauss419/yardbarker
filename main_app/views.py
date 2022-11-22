524f8e83ba5576ebb56b14a966f00d884edc6590
ProSweeper <dylan.doucet28@gmail.com>
2022-11-22 18:49:04 +0000

    add contractor list and detail pages and functionality

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ import uuid
 import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -40,6 +41,13 @@ class JobUpdate(LoginRequiredMixin, UpdateView):
 class JobDelete(LoginRequiredMixin, DeleteView):
   model = Job
   success_url = '/jobs'
+
+class ContractorList(LoginRequiredMixin, ListView):
+  model = Contractor
+
+class ContractorDetail(LoginRequiredMixin, DetailView):
+  model = Contractor
+  
 
 
 @login_required
